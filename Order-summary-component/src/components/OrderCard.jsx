0ca0f28a59bc2bc7c3615f3767211ebd3bde4c45
wishfulgenie <aleemertariq@gmail.com>
2024-05-22 14:38:01 -0700

    Fixed background banner image issue.

--- Order-summary-component/src/components/OrderCard.jsx
+++ Order-summary-component/src/components/OrderCard.jsx
@@ -1,29 +1,20 @@
 /** SVG Imports */
 import BannerImage from "../../assets/images/illustration-hero.svg"
-import MusicLogo from "../../assets/images/icon-music.svg"
 
 /** Component Imports */
+import OrderPlan from "../components/OrderPlan"
 import Button from "../components/Button"
 
 const OrderCard = () => {
   return (
-    <div className="order-card">
+    <div className="card-and-banner">
       <img className="order-banner" src={BannerImage} />
-      <div className="order-summary">
-        <div className="order-summary-text">
+      <div className="order-card">
+        <div className="order-summary">
           <h1> Order Summary </h1>
           <p> You can now listen to millions of songs, audiobooks, and podcasts on any device anwhere you like! </p>
         </div>
-        <div className="order-plan">
-          <div className="plan-details">
-            <img width="45px" height="45px" src={MusicLogo} />
-            <div className="plan-details-text">
-              <h4>Annual Plan</h4>
-              <p>$59.99/year</p>
-            </div>
-          </div>
-          <Button label={"Change"} className={"change-button"} />
-        </div>
+        <OrderPlan />
         <div className="button-display">
           <Button label={"Proceed to Payment"} className={"payment-button"} />
           <Button label={"Cancel Order"} className={"cancel-button"} />
