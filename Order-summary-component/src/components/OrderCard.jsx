c81cd37b39e569a47d853aee08e764d20725605b
wishfulgenie <aleemertariq@gmail.com>
2024-05-22 14:50:41 -0700

    Completed.

--- Order-summary-component/src/components/OrderCard.jsx
+++ Order-summary-component/src/components/OrderCard.jsx
@@ -3,6 +3,7 @@ import BannerImage from "../../assets/images/illustration-hero.svg"
 
 /** Component Imports */
 import OrderPlan from "../components/OrderPlan"
+import OrderSummary from "../components/OrderSummary"
 import Button from "./Button"
 import Image from "./Image"
 
@@ -11,10 +12,7 @@ const OrderCard = () => {
     <div className="card-and-banner">
       <Image src={BannerImage} className={"order-banner"} />
       <div className="order-card">
-        <div className="order-summary">
-          <h1> Order Summary </h1>
-          <p> You can now listen to millions of songs, audiobooks, and podcasts on any device anwhere you like! </p>
-        </div>
+        <OrderSummary />
         <OrderPlan />
         <div className="button-display">
           <Button label={"Proceed to Payment"} className={"payment-button"} />
