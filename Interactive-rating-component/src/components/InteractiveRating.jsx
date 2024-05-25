aea64f0a862f15da74623ffe0b5f8238fa431466
wishfulgenie <aleemertariq@gmail.com>
2024-05-24 22:09:02 -0700

    Minor changes, also setup basic theme provider for styled components.

---
+++ Interactive-rating-component/src/components/InteractiveRating.jsx
@@ -0,0 +1,9 @@
+const InteractiveRating = () => {
+  return (
+    <div>
+      <p> Hello </p>
+    </div>
+  )
+}
+
+export default InteractiveRating;
--- Interactive-rating-component/src/main.jsx
+++ Interactive-rating-component/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { ThemeProvider } from 'styled-components'
+import InteractiveRating from './components/InteractiveRating.jsx'
+import { theme } from './theme.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <InteractiveRating />
+    </ThemeProvider>
   </React.StrictMode>,
 )
