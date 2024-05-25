157f751e788d6de5f80a3a4e390b1606f3468cb6
wishfulgenie <aleemertariq@gmail.com>
2024-05-25 12:12:23 -0700

    Basic card setup started.

--- Interactive-rating-component/src/components/App.jsx
+++ Interactive-rating-component/src/components/App.jsx
@@ -1,9 +1,10 @@
-const InteractiveRating = () => {
+const App = () => {
   return (
-    <div>
-      <p> Hello </p>
+    <div className="interactive-rating-display">
+      <h2 className="display-title">How did we do?</h2>
+      <p className="display-message">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
     </div>
   )
 }
 
-export default InteractiveRating;
+export default App;
