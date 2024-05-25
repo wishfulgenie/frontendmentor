5a68212a86d235b1dde7442e46f8b5fb0da99dd8
wishfulgenie <aleemertariq@gmail.com>
2024-05-25 13:35:45 -0700

    Using map for rating buttons.

--- Interactive-rating-component/src/components/App.jsx
+++ Interactive-rating-component/src/components/App.jsx
@@ -6,6 +6,8 @@ import Button from './Button';
 const App = () => {
   const [rating, setRating] = useState(null);
 
+  const ratings = [1, 2, 3, 4, 5]
+
   const handleUpdateRating = (newRating) => {
     setRating(newRating)
   }
@@ -17,11 +19,15 @@ const App = () => {
         <p className="display-message">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       </div>
       <div className="rating-buttons">
-        <Button label={1} type="rate" active={rating==1} onUpdateRating={handleUpdateRating} />
-        <Button label={2} type="rate" active={rating==2} onUpdateRating={handleUpdateRating} />
-        <Button label={3} type="rate" active={rating==3} onUpdateRating={handleUpdateRating} />
-        <Button label={4} type="rate" active={rating==4} onUpdateRating={handleUpdateRating} />
-        <Button label={5} type="rate" active={rating==5} onUpdateRating={handleUpdateRating} />
+      {ratings.map((value) => (
+        <Button
+          key={value}
+          label={value}
+          type="rate"
+          active={rating === value}
+          onUpdateRating={handleUpdateRating}
+        />
+      ))}
       </div>
       <Button label={"Submit"} type="submit" active={false} />
     </div>
