aa14afc10efafb92777a12b904b8b3e42a4ddc61
wishfulgenie <aleemertariq@gmail.com>
2024-05-25 13:12:49 -0700

    Finished submit button, margin workaround for spacing; need to find a better approach.

--- Interactive-rating-component/src/components/App.jsx
+++ Interactive-rating-component/src/components/App.jsx
@@ -9,12 +9,13 @@ const App = () => {
         <p className="display-message">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       </div>
       <div className="rating-buttons">
-        <Button type="rate">1</Button>
-        <Button type="rate">2</Button>
-        <Button type="rate">3</Button>
-        <Button type="rate">4</Button>
-        <Button type="rate">5</Button>
+        <Button type="rate"><span> 1 </span></Button>
+        <Button type="rate"><span> 2 </span></Button>
+        <Button type="rate"><span> 3 </span></Button>
+        <Button type="rate"><span> 4 </span></Button>
+        <Button type="rate"><span> 5 </span></Button>
       </div>
+      <Button type="submit"><span> Submit </span></Button>
     </div>
   )
 }
