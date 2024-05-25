bb7ef12a53795c46cbbdda2d135c6facd97f7dfe
wishfulgenie <aleemertariq@gmail.com>
2024-05-25 13:59:44 -0700

    Abstracted out Rating component.

--- Interactive-rating-component/src/components/App.jsx
+++ Interactive-rating-component/src/components/App.jsx
@@ -1,48 +1,13 @@
 import { useState } from 'react';
 
-/** Component Imports */
-import Button from './Button';
-import Image from './Image';
-
-/** Image Imports */
-import Star from '../../assets/images/icon-star.svg'
+/* Component Imports */
+import Rating from './Rating'
 
 const App = () => {
-  const [rating, setRating] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-
-  const ratings = [1, 2, 3, 4, 5]
-
-  const handleUpdateRating = (newRating) => {
-    setRating(newRating)
-  }
-
-  const handleUpdateSubmission = () => {
-    setSubmitted(true);
-  }
-
   return (
-    <div className="interactive-rating-display">
-      <div className='star-image'>
-        <Image src={Star} width={"15"} height={"15"}/>
-      </div>
-      <div className="rating-message">
-        <h2 className="display-title">How did we do?</h2>
-        <p className="display-message">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-      </div>
-      <div className="rating-buttons">
-      {ratings.map((value) => (
-        <Button
-          key={value}
-          label={value}
-          type="rate"
-          active={rating === value}
-          onClick={handleUpdateRating}
-        />
-      ))}
-      </div>
-      <Button label={"Submit"} type="submit" active={false} onClick={handleUpdateSubmission} />
-    </div>
+    <>
+      <Rating />
+    </>
   )
 }
 
---
+++ Interactive-rating-component/src/components/Rating.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+/* Styling imports */
+import styles from './Rating.module.css'
+
+/* Component Imports */
+import Button from './Button';
+import Image from './Image';
+
+/* Image Imports */
+import Star from '../../assets/images/icon-star.svg'
+
+const Rating = () => {
+  const [rating, setRating] = useState(null);
+
+  const ratings = [1, 2, 3, 4, 5]
+
+  const handleUpdateRating = (newRating) => {
+    setRating(newRating)
+  }
+
+  return (
+    <div className={styles['interactive-rating-display']}>
+      <div className={styles['star-image']}>
+        <Image src={Star} width={"15"} height={"15"}/>
+      </div>
+      <div className={styles['rating-message']}>
+        <h2 className={styles['display-title']}>How did we do?</h2>
+        <p className={styles['display-message']}>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+      </div>
+      <div className={styles['rating-buttons']}>
+      {ratings.map((value) => (
+        <Button
+          key={value}
+          label={value}
+          type="rate"
+          active={rating === value}
+          onClick={handleUpdateRating}
+        />
+      ))}
+      </div>
+      <Button label={"Submit"} type="submit" active={false} />
+    </div>
+  )
+}
+
+export default Rating;
