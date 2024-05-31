b1c0500868a3c0b997667edefa65996f2585fb2f
wishfulgenie <aleemertariq@gmail.com>
2024-05-31 16:39:21 -0700

    Updated json data.

--- Job-listings-app/src/components/App.jsx
+++ Job-listings-app/src/components/App.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+
+/* Data Import */
+import data from '../data.json'
+
 const App = () => {
+  const [jobList, setJobList] = useState(data)
+
+  console.log(jobList);
+
   return (
     <div>
       <p> Hello </p>
