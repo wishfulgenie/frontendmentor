27dd6ba10b8c1c7179bfa1ad7ebfba87c2fa1778
wishfulgenie <aleemertariq@gmail.com>
2024-05-31 18:10:39 -0700

    Partway through handling the JobCard.

--- Job-listings-app/src/components/JobCard/JobCard.jsx
+++ Job-listings-app/src/components/JobCard/JobCard.jsx
@@ -1,10 +1,25 @@
 /* Styling imports */
 import styles from './JobCard.module.css'
 
+const JobCardCompany = ({ company, newJob, featuredJob }) => {
+  return (
+    <div className={styles['job-card-company']}>
+      <p className={styles['company-role']}> {company} </p>
+        {newJob && <span className={styles['new-status']}>new!</span>}
+        {featuredJob && <span className={styles['featured-status']}>featured</span>}
+    </div>
+  )
+}
+
 const JobCard = ({ job }) => {
   return (
-    <div>
-      <p> listing </p>
+    <div className={styles['job-card']}>
+      <div className={styles['job-details-and-image']}>
+        <img src={`../../assets/images/${job.logo}`} />
+        <div className={styles['job-details']}>
+          <JobCardCompany company={job.company} newJob={job.new} featuredJob={job.featured}/>
+        </div>
+      </div>
     </div>
   )
 }
