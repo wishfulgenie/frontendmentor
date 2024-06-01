a59287c08ff2121897b86de3fbbb249ce1c25fed
wishfulgenie <aleemertariq@gmail.com>
2024-06-01 12:06:09 -0700

    Updated clickable, the ClickableChip doesn't need 'clicked' status anymore.

--- Job-listings-app/src/atoms/ClickableChip.jsx
+++ Job-listings-app/src/atoms/ClickableChip.jsx
@@ -6,14 +6,11 @@ import removeIcon from '../../assets/images/icon-remove.svg'
 
 const ClickableChip = ({ label, type, clicked, onClickHandler }) => {
 
-  const renderAddFilter = (clicked) => {
-    const className = clicked ? styles['clicked'] : styles['clickable']
-    const action = clicked ? 'remove' : 'add';
-
+  const renderAddFilter = () => {
     return (
       <div
-        className={className}
-        onClick={(e) => onClickHandler(e, action, label)}
+        className={styles['clickable']}
+        onClick={(e) => onClickHandler(e, 'add', label)}
       >
         {label}
       </div>
