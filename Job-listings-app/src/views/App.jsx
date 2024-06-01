ba51f658f12f0b031d150f61a0b471266e63cd41
wishfulgenie <aleemertariq@gmail.com>
2024-06-01 12:23:38 -0700

    Almost completed, need to fix minor stylistic issues, will remove requirement for mobile-design.

--- Job-listings-app/src/views/App.jsx
+++ Job-listings-app/src/views/App.jsx
@@ -4,37 +4,62 @@ import { useState } from 'react'
 import data from '../data.json'
 
 /* Component Imports */
+import JobFilterPanel from '../organisms/JobFilterPanel'
 import JobListing from '../templates/JobListing'
 
 const App = () => {
   const [jobList, setJobList] = useState(data)
-  const [filterSelection, setFilterSelection] = useState(
-    ['TypeScript', 'React', 'JavaScript', 'Python', 'Full Stack', 'CSS']
-  )
+  const [filterSelection, setFilterSelection] = useState([])
+
+  const displayFilterPanel = filterSelection.length !== 0
 
   // TODO: Limit to a maximum of 8 filters
   const updateFilterSelection = (e, action, data) => {
     e.preventDefault()
     console.log(`action: ${action}, data: ${data}`)
-    if (action === 'add') {
-      if (!filterSelection.includes(data)) {
-        console.log('added')
-        setFilterSelection(filterSelection.concat(data))
-      }
-    } else if (action === 'remove') {
-      setFilterSelection(filterSelection.filter((sel) => sel !== data))
-    } else {
-      setFilterSelection([])
+
+    if (action === 'add' && filterSelection.length === 8) {
+      console.log('exceeded 8 filters, this is an issue')
+      return
+    }
+
+    switch (action) {
+      case 'add':
+        if (!filterSelection.includes(data)) {
+          console.log(`added ${data}`)
+          setFilterSelection(filterSelection.concat(data))
+        }
+        break;
+      case 'remove':
+        const newSelection = filterSelection.filter((s) => s !== data)
+        setFilterSelection(newSelection)
+        break;
+      case 'clear':
+        setFilterSelection([])
+        break;
+      default:
+        break;
     }
   }
 
   return (
     <div className='job-app'>
-      <JobListing 
-        job={jobList[0]} 
-        filterSelection={filterSelection} 
-        updateFilterSelection={updateFilterSelection}
-      />
+      {displayFilterPanel &&
+        <JobFilterPanel 
+          filterSelection={filterSelection}
+          updateFilterSelection={updateFilterSelection}
+        />
+      }
+      {jobList.map(
+        (job) => 
+          <JobListing
+            key={job.id} 
+            job={job} 
+            filterSelection={filterSelection} 
+            updateFilterSelection={updateFilterSelection}
+          />
+      )}
+      
     </div>
   )
 }
