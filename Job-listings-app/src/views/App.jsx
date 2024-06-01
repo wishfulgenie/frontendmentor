7b2ec3dd38c9f772dc3903f67921c11ba3851494
wishfulgenie <aleemertariq@gmail.com>
2024-06-01 02:16:33 -0700

    Created Chip, with all respective styling for chips.

--- Job-listings-app/src/views/App.jsx
+++ Job-listings-app/src/views/App.jsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 /* Data Import */
 import data from '../data.json'
 
+/* Component Imports */
+import Chip from '../atoms/Chip'
+
 const App = () => {
   const [jobList, setJobList] = useState([data[0]])
   const [filterSelection, setFilterSelection] = useState([])
 
   return (
     <div className='job-app'>
-      
+      <p> Hello </p>
     </div>
   )
 }
