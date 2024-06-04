3379e89da97daf3701cf12d6ab3dad0d592cb23d
wishfulgenie <aleemertariq@gmail.com>
2024-06-03 19:03:23 -0700

    Created Input component.

--- Age-calculator-app/src/views/App.jsx
+++ Age-calculator-app/src/views/App.jsx
@@ -1,12 +1,29 @@
 /* Component Imports */
 import Chip from '../atoms/Chip'
+import Input from '../atoms/Input'
+
 
 const App = () => {
+  const testFunction = (e, dateLabel) => {
+    console.log(`event value: ${e.target.value}, dateLabel: ${dateLabel}`)
+  }
+
   return (
     <div className="age-calculator">
-      <Chip
+      <Chip 
+        label={'Month'}
         type={'input'}
-        label={'years'}
+        errorStatus={true}
+      />
+      <Input
+        placeholder={'DD'}
+        value={'22'}
+        errorStatus={true}
+        onChangeHandler={testFunction}
+      />
+      <Chip 
+        label={'Must be a valid month'}
+        type={'error'}
       />
     </div>
   )
