fc2068881e3879d44ce88a731d501b39da1006a9
wishfulgenie <aleemertariq@gmail.com>
2024-06-05 03:14:06 -0700

    created Button component.

--- Order-summary-component/src/views/App.jsx
+++ Order-summary-component/src/views/App.jsx
@@ -1,6 +1,7 @@
 /* Component Imports */
 import Chip from '../atoms/Chip.jsx'
 import ClickableChip from '../atoms/ClickableChip.jsx'
+import Button from '../atoms/Button.jsx'
 
 const App = () => {
 
@@ -15,6 +16,16 @@ const App = () => {
       <Chip label={'Annual Plan'} className={'alt-header-text'}/>
       <Chip label={'$59.99/year'} className={'regular-text'}/>
       <ClickableChip label={'Change'} onClickHandler={testfn}/>
+      <Button 
+        label={'Proceed to Payment'} 
+        className={'payment-button'} 
+        onClickHandler={testfn}
+      />
+      <Button 
+        label={'Cancel Order'} 
+        className={'cancel-button'} 
+        onClickHandler={testfn}
+      />
     </div>
   )
 }
