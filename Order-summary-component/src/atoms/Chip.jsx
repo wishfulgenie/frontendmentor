fa59d43eac4faa9030bdf7024aea628abe731f3f
wishfulgenie <aleemertariq@gmail.com>
2024-06-05 02:35:52 -0700

    Created Chip component.

---
+++ Order-summary-component/src/atoms/Chip.jsx
@@ -0,0 +1,12 @@
+/* Styling imports */
+import styles from './Chip.module.css'
+
+const Chip = ({ label, className }) => {
+  return (
+    <div className={styles[className]}>
+      {label}
+    </div>
+  )
+}
+
+export default Chip
--- Order-summary-component/src/views/App.jsx
+++ Order-summary-component/src/views/App.jsx
@@ -1,7 +1,12 @@
+/* Component Imports */
+import Chip from '../atoms/Chip.jsx'
+
 const App = () => {
   return (
-    <div>
-      <p> Hello </p>
+    <div className="order-card-container">
+      <Chip label={'Order Summary'} className={'header-text'}/>
+      <Chip label={'Annual Plan'} className={'alt-header-text'}/>
+      <Chip label={'$59.99/year'} className={'regular-text'}/>
     </div>
   )
 }
